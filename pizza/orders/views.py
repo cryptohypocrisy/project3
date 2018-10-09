012b6d5b6ee83129c038565327bc8fee9df9519e
Imirie <DImirie@smithbucklin.com>
2018-10-09 16:46:10 -0400

    working signup/login

--- pizza/orders/views.py
+++ pizza/orders/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.urls import reverse
+from django.db.utils import IntegrityError
 
 # Create your views here.
 def index(request):
@@ -15,8 +17,8 @@ def index(request):
 
 
 def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
+    username = request.POST['login-username']
+    password = request.POST['login-password']
 
     user = authenticate(username=username, password=password)
 
@@ -29,8 +31,22 @@ def login_view(request):
 def logout_view(request):
 
     context = {
-        'user': request.user,
+        'user': request.user, # need this to display a custom logout message to user
     }
 
     logout(request)
     return render(request, 'orders/logout.html', context, {'message': 'Logged out!'})
+
+
+def signup_view(request):
+    first_name = request.POST["signup-first_name"]
+    last_name = request.POST["signup-last_name"]
+    email = request.POST["signup-email"]
+    password = request.POST["signup-password"]
+
+    try:
+        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=email, email=email, password=password)
+    except IntegrityError:
+        return render(request, 'orders/login.html', {'message': 'Username already taken, pick another...'})
+
+    return render(request, 'orders/newuser.html', {'message': 'Thanks for creating an account', 'user': first_name})
